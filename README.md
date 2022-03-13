d34815d2ba039f14fd7dbe73ac4fbf9001f62bff
Sean Breckenridge <seanbrecke@gmail.com>
2022-03-13 13:33:18 -0700

    allow iterators as inputs, options as args/kwargs
    add CI, testing, mypy, update docs

--- README.md
+++ README.md
@@ -12,9 +12,9 @@ Requirements
 ------------
 
 * Python 3.6+
-* [fzf](https://github.com/junegunn/fzf)
+* [`fzf`](https://github.com/junegunn/fzf)
 
-*Note*: fzf must be installed and available on PATH.
+*Note*: `fzf` must be installed and available on PATH.
 
 Installation
 ------------
@@ -22,26 +22,51 @@ Installation
 
 Usage
 -----
-    >>> from pyfzf.pyfzf import FzfPrompt
-    >>> fzf = FzfPrompt()
+
+```python
+from pyfzf.pyfzf import FzfPrompt
+
+fzf = FzfPrompt()
+fzf = FzfPrompt(default_options="--reverse")
+```
 
 If `fzf` is not available on PATH, you can specify a location
 
-    >>> fzf = FzfPrompt('/path/to/fzf')
+```python
+fzf = FzfPrompt('/path/to/fzf')
+```
 
-Simply pass a list of options to the prompt function to invoke fzf.
+Simply pass a sequence of items to the prompt function to invoke `fzf`
 
-    >>> fzf.prompt(range(0,10))
+```python
+fzf.prompt(range(0,10))
+```
 
-You can pass additional arguments to fzf as a second argument
+You can pass additional positional arguments to `fzf`.
 
-    >>> fzf.prompt(range(0,10), '--multi --cycle')
+```python
+fzf.prompt(range(0,10), '--multi', '--cycle')
+fzf.prompt(range(0,50), 'multi', 'cycle', height='20%')
+fzf.prompt(range(0,50), 'x', 'i', 'm', '--tac')
+```
 
-Input items are written to a temporary file which is then passed to fzf.
-The items are delimited with `\n` by default, you can also change the delimiter
-(useful for multiline items)
+Items are streamed to the `fzf` process one line at a time, you can pass
+any sort of iterator or generator as the first argument. For example, a file object,
+or a glob of files to search for, displaying a preview:
 
-    >>> fzf.prompt(range(0,10), '--read0', '\0')
+```python
+fzf.prompt(open("README.md"), "-m", delimiter="")
+
+from pathlib import Path
+fzf.prompt(Path(".").rglob("*.md"), "-m", r"--preview='cat {}'")
+```
+
+Items are delimited with `\n` by default, you can also change the delimiter (useful for multiline items):
+
+```python
+>>> fzf.prompt(["5\n10", "15\n20"], '--read0', '-m', delimiter='\0')
+['15\n20']
+```
 
 License
 -------
--- pyfzf/pyfzf.py
+++ pyfzf/pyfzf.py
@@ -22,17 +22,28 @@
 #
 # Author: Nagarjuna Kumarappan <nagarjuna.412@gmail.com>
 
+import shlex
+import subprocess
 from shutil import which
-import os
-import tempfile
+from typing import Optional, Sequence, Any, Union, Iterable, List, Iterator
 
 
 # constants
 FZF_URL = "https://github.com/junegunn/fzf"
 
 
+# types
+# either just a string like "--reverse --bold"
+# or a sequence (list or tuple) of keys or key value pairs
+# e.g. ("--no-mouse", "-m", ("--height, 50))
+FzfOptions = Sequence[Union[str, Sequence[str]]]
+InputSequence = Union[Sequence[Any], Iterable[Any], Iterator[Any]]
+
+
 class FzfPrompt:
-    def __init__(self, executable_path=None):
+    def __init__(self,
+                 executable_path: Optional[str] = None,
+                 default_options: FzfOptions = ()):
         if executable_path:
             self.executable_path = executable_path
         elif not which("fzf") and not executable_path:
@@ -41,27 +52,113 @@ class FzfPrompt:
         else:
             self.executable_path = "fzf"
 
-    def prompt(self, choices=None, fzf_options="", delimiter='\n'):
-        # convert lists to strings [ 1, 2, 3 ] => "1\n2\n3"
-        choices_str = delimiter.join(map(str, choices))
+        self.options: List[str] = self.__class__.parse_options(default_options)
+
+    def __repr__(self) -> str:
+        return f'FzfPrompt(executable_path={self.executable_path}, default_options={repr(" ".join(self.options))})'
+
+    @staticmethod
+    def key_to_option(key: str) -> str:
+        if key.startswith('-'):
+            # user passed something like --preview or -x already, dont prepend hyphens
+            return key
+        else:
+            # compute fzf shell option
+            if len(key) == 1:
+                # e.g. x --> -x
+                return f'-{key}'
+            else:
+                # e.g. preview -> --preview
+                return f'--{key}'
+
+    @staticmethod
+    def parse_options(options: FzfOptions) -> List[str]:
+        '''Parses arbitrary options to a list of options for fzf'''
+        if isinstance(options, str):
+            if options.strip() == '':
+                return []
+            # if user passed a string with no spaces, try to add hyphens
+            if " " not in options:
+                return [FzfPrompt.key_to_option(options)]
+            else:
+                # assume the user passed a constructed string of options
+                return [options]
+        else:
+            computed_options = []
+            for opt in options:
+                key: str
+                value: Optional[str] = None
+                # user passed single item
+                if isinstance(opt, str):
+                    if opt.strip() == '':
+                        continue
+                    key = opt
+                elif isinstance(opt, Sequence):
+                    if len(opt) != 2:
+                        raise TypeError(f'Expected a tuple or list with two items, received {len(opt)} "{opt}"')
+                    if not isinstance(opt[0], str):
+                        raise TypeError(f'Expected a str key, received {opt[0]}')
+                    key = opt[0]
+                    value = str(opt[1])  # use could pass something else, e.g. int
+                else:
+                    raise TypeError(f'Expected a str or a sequence of options, e.g., ["-x", ("height", "40%")], received {opt}')
+
+                if value is not None and value.strip():
+                    # e.g. --info=default --margin="TRBL"
+                    # shlex.quote to prevent double quotes from breaking command
+                    computed_options.append(f"{FzfPrompt.key_to_option(key)}={shlex.quote(value)}")
+                else:
+                    # option without a value, e.g. --reverse,
+                    computed_options.append(FzfPrompt.key_to_option(key))
+            return computed_options
+
+    def prompt(self,
+               choices: Union[Sequence[Any], Iterable[Any], Iterator[Any]],
+               *args: FzfOptions,
+               delimiter: str = '\n',
+               encoding='utf-8',
+               **kwargs: Any) -> Any:
+
+        # combine args/kwargs into fzf options
+        opts_raw: List[Any] = list(args)
+        for k, v in kwargs.items():
+            opts_raw.append((k, v))
+
+        # parse into options
+        opts = self.__class__.parse_options(opts_raw)
+
+        # add any options set on the Fzf instance
+        opts.extend(self.options)
+        options =  ' '.join(opts)
+
+        selected_text: str = ''
         selection = []
 
-        with tempfile.NamedTemporaryFile(delete=False) as input_file:
-            with tempfile.NamedTemporaryFile(delete=False) as output_file:
-                # Create an temp file with list entries as lines
-                input_file.write(choices_str.encode('utf-8'))
-                input_file.flush()
+        # spawn a process and send lines one at a time
+        # https://stackoverflow.com/a/69397677
+        fzf_process = subprocess.Popen(
+            shlex.split(f'{self.executable_path} {options}'),
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            encoding=encoding,
+        )
+        assert fzf_process.stdin is not None, 'Fatal error creating input stream'
+        try:
+            for item in choices:
+                sitem = str(item)  # iterator could be anything, convert to string
+                fzf_process.stdin.write(sitem)
+                fzf_process.stdin.write(delimiter)
+        except BrokenPipeError:
+            pass
+        stdout, _ = fzf_process.communicate()
+        if stdout != '':
+            selected_text = stdout
 
-        # Invoke fzf externally and write to output file
-        os.system(
-            f"{self.executable_path} {fzf_options} < \"{input_file.name}\" > \"{output_file.name}\"")
+        # can't split by '', default to newlines if misconfigured
+        use_delimiter = '\n' if delimiter == '' else delimiter
 
-        # get selected options
-        with open(output_file.name, encoding="utf-8") as f:
-            for line in f:
-                selection.append(line.strip('\n'))
-
-        os.unlink(input_file.name)
-        os.unlink(output_file.name)
+        if selected_text:
+            selection = selected_text.rstrip('\n').split(use_delimiter)
 
         return selection
+
